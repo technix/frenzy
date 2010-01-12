01d549203ce31c68c214a5c8741be36f9decaa5f
Sergei Mozhaisky <technix@frenzy.org.ua>
2010-01-12 12:00:42 +0000

    Updated Frenzy boot loader build tools (now 8.0-R compatible)

--- buildscripts/stages/boot/make.sh
+++ buildscripts/stages/boot/make.sh
@@ -6,7 +6,6 @@ CRBIN=frenzy_loader
 
 . ../../config
 
-
 # removing old frenzyroot
 rm frenzyroot.gz > /dev/null 2>&1
 
@@ -34,14 +33,14 @@ rm -f crunch.mk > /dev/null 2>&1
 cd ..
 fi
 
-BC=2048
+BC=4096
 #BC=1536
 BS=8192
 dd if=/dev/zero of=frenzyroot count=${BC} bs=1k  > /dev/null 2>&1
 mdconfig -a -t vnode -f frenzyroot -u ${DEV} > /dev/null 2>&1
 bsdlabel -w md${DEV} auto  > /dev/null 2>&1
-newfs -n -i ${BS} -m 0 -o space /dev/md${DEV}c  > /dev/null 2>&1
-mount /dev/md${DEV}c ${DIR} 
+newfs -n -i ${BS} -m 0 -o space /dev/md${DEV}  > /dev/null 2>&1
+mount /dev/md${DEV} ${DIR}
 
 echo -n " dirs"
 rm -r ${DIR}/* 2>/dev/null
